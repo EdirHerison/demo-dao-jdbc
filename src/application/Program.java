7b34d837cd01bf25f205625506e54ac78b28466a
Edir Herison <edir_h@hotmail.com>
2022-09-08 17:57:31 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,7 +15,7 @@ public class Program {
 		Department obj = new Department(1, "Books");
 		Seller sel = new Seller(1,"Edir Herison", "edir.ti@minhaempresa.com.br",
 				new Date(),3500.0, obj);
-		
+		SellerDao sldt = DaoFactory.createSellerDao();
 	
 		System.out.print(sel);
 
