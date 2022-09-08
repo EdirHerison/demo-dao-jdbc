3b37d0b9caace1d47ca62448bd059e6bfa395b64
Edir Herison <edir_h@hotmail.com>
2022-09-08 17:32:40 -0300

    Entiti Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,21 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); 
 		Department obj = new Department(1, "Books");
+		Seller sel = new Seller(1,"Edir Herison", "edir.ti@minhaempresa.com.br",
+				new Date(),3500.0, obj);
 		
-		System.out.println(obj);
+	
+		System.out.print(sel);
 
 	}
 
